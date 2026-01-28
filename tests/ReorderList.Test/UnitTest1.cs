44f5359ded9557ee175c76b86d44aaaf4674355f
AlexBh1304 <AlexBh1304@users.noreply.github.com>
2026-01-28 09:00:20 -0700

    Added test cases

---
+++ tests/ReorderList.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using core;
+namespace ReorderList.Test;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData("bbbbb",1)]
+    [InlineData("abcabcbb",3)]
+    [InlineData("pwwkew",3)]
+    public void TestCasesReorderList(string s, int expected)
+    {
+        var resultado = Solution.LengthOfLongestSubstring(s);
+        Assert.Equal(expected, resultado);
+    }
+}
